a88c23aa66472d156da7092959fb1ac500294f38
Felipe Printes <felipe.unilasalle@gmail.com>
2017-09-11 17:50:17 -0300

    updates

--- MyFirstAplication/app/src/main/java/com/aplicacao/felip/myfirstaplication/AvaliacaoFilme.java
+++ MyFirstAplication/app/src/main/java/com/aplicacao/felip/myfirstaplication/AvaliacaoFilme.java
@@ -2,14 +2,26 @@ package com.aplicacao.felip.myfirstaplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AvaliacaoFilme extends AppCompatActivity {
+
+    private Button botaoAvalia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avaliacao_filme);
 
+        Button botaoAvalia = (Button) findViewById(R.id.botao_avaliar);
+        botaoAvalia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AvaliacaoFilme.this, "Avaliação realizada com sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
--- MyFirstAplication/app/src/main/java/com/aplicacao/felip/myfirstaplication/MainActivity.java
+++ MyFirstAplication/app/src/main/java/com/aplicacao/felip/myfirstaplication/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView ImgBotao;
+    private Button botaoAdiciona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        botaoAdiciona = (Button) findViewById(R.id.botao_adiciona);
+        botaoAdiciona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Botao de adicionar clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
